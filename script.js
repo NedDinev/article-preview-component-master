7538b6c983ad8a13dfec4e23b48ab5a06ec229b9
Nedelcho Dinev <ned.dinev@gmail.com>
2021-12-22 18:14:01 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+let button = document.getElementById(`share_icon`);
+let social = document.getElementById(`social_hide`);
+button.addEventListener(`click`, function () {
+  social.style = `display: unset;`;
+});
